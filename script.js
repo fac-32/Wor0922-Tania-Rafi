66472d8dddf880e727af9e8c47004d936a30a493
krilklem <rafi.panyotova@gmail.com>
2025-09-22 15:55:09 +0100

    Make HTML semantic

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const canvasSection = document.createElement("section");
 canvasSection.classList.add("content-section");
 canvasSection.style.display = "flex";
 canvasSection.style.justifyContent = "center";
-document.querySelector("#main").appendChild(canvasSection);
+document.querySelector("main").appendChild(canvasSection);
 
 const canvas = document.createElement("canvas");
 canvas.setAttribute("width", "700");
